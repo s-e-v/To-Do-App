8192cf751d74083cfa8359486f1bd4cd0ff3251c
Severino <seve.gu@gmail.com>
2017-08-27 13:04:20 +0100

    press Enter or click the buttton on list to-dos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,24 @@
-$("li").click(function () { 
+$("ul").on("click", "li", function () { 
     $(this).toggleClass("final");
 });
 
-
-$("span").click(function(){
+$("ul").on("click", "span", function(event){
     $(this).parent().fadeOut(500,function(){
         $(this).remove();
     });
     event.stopPropagation();
-})
+});
+
+$("input[type='text']").keypress(function(event) { 
+    if(event.which === 13){
+        var text = $(this).val();
+        $(this).val("");
+        $("ul").append("<li>"+text+" <span>X</span></li>");
+    };
+});
+
+$("#button").click(function () {
+    var text = $("input[type='text']").val();
+    $("input[type='text']").val("");
+    $("ul").append("<li>"+text+" <span>X</span></li>");
+});
